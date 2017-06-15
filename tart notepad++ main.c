87e65ed2d6e306aa8afa0e046298c888701d03b2
Harris Mohsin <Harris Mohsin>
2017-06-15 14:26:16 -0400

    Initialized item list

---
+++ tart notepad++ main.c
@@ -0,0 +1,291 @@
+[33mcommit 9de0a393f6dc6ff9fa884d7c1bf0229ed707cdbb[m
+Merge: fbae826 7abf38d
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Mon Jun 12 14:46:38 2017 -0400
+
+    Merge branch 'master' of https://github.com/HarrisMohsin/EndlessBattle
+
+[33mcommit fbae826771edb1479f3f50b2ad239c04ff96f0d1[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Mon Jun 12 14:46:33 2017 -0400
+
+    item list started
+
+[33mcommit 7abf38d0a3eb31186904ba5edf6c4e430eff10e9[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Mon Jun 12 14:42:10 2017 -0400
+
+    debugging, 1 bug left
+
+[33mcommit 472c80440d1cb81dc02cc37af7d91ca999f65bf9[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Sun Jun 4 13:25:54 2017 -0400
+
+    beautified main.c
+
+[33mcommit 7e7666489169d0d19fa2ed0426cabd0971769ad4[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Fri Jun 2 13:58:48 2017 -0400
+
+    stats added
+
+[33mcommit 4e3b54c9894fd898b3c4787ffc6f83142e507301[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Fri May 26 00:07:44 2017 -0400
+
+    uncommited wake changes
+
+[33mcommit 5de625885644114fcf974186c2289f5afafd2eac[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed May 3 14:49:08 2017 -0400
+
+    debugging; bugs located; to be obliterated
+
+[33mcommit f6de81d1e98fe56cc6cbaef1e275fdf1dd9fef32[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Wed May 3 14:35:56 2017 -0400
+
+    OIWSOM
+
+[33mcommit 72c7f4bce619383a32a9f07f9dee425912931ca8[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Thu Apr 20 18:29:41 2017 -0400
+
+    debugged  wake
+
+[33mcommit 988e3d551130b34c36f70404b2136e40a91db4f3[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Apr 19 14:47:07 2017 -0400
+
+    wake failed
+
+[33mcommit 2d660ff9c8efa1831716db6f1421d980b04fc5b0[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Wed Apr 19 14:45:49 2017 -0400
+
+    Star file
+
+[33mcommit 868beecc9253a34aa30f39348b581ee2a141bf2b[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Apr 19 13:22:10 2017 -0400
+
+    made makefile
+
+[33mcommit 6e8421d78e5859b0d5005b5e93e536cbb6661593[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Apr 19 13:16:22 2017 -0400
+
+    added wake
+
+[33mcommit bf9dab274d66bdffea3a67dfd9c2d9830133eee3[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Wed Apr 12 14:48:47 2017 -0400
+
+    defined a new function
+
+[33mcommit ebd7e45daf14fd4700115d9d4bea0ef90fd9aa6b[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Apr 12 14:20:13 2017 -0400
+
+    made star.c star.h
+
+[33mcommit e4c0b171a6632c5afbb6b1e740a4da8b30ad5378[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Apr 12 13:54:57 2017 -0400
+
+    debug name prompt
+
+[33mcommit ac5a3f5028695f611cb6e22513c53b6ba79adfe4[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Apr 12 13:51:57 2017 -0400
+
+    fixed randrange bug
+
+[33mcommit 6b1a9561bcc6e9781bd5adcc266625a86ca38035[m
+Merge: b7a1a80 18d4009
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Wed Apr 5 14:29:43 2017 -0400
+
+    Ok
+
+[33mcommit b7a1a80228230454bc6fc69b7c973001869a2d25[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Wed Apr 5 14:24:15 2017 -0400
+
+    Resolving merge
+
+[33mcommit 18d40094a34b33be9f3b68fffccff445334338e3[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Apr 5 14:07:54 2017 -0400
+
+    added ignore file
+
+[33mcommit 687d20bf6631d51a375df8abd36fe13ec36e1dc7[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Apr 5 14:07:08 2017 -0400
+
+    removed exe file'
+
+[33mcommit a1f579bda33c47f485d952ab2241973c90c51668[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Wed Apr 5 13:39:15 2017 -0400
+
+    Added initialization input
+
+[33mcommit fff4d4a1416eb9b8c52808225f7e1f9d5eba43a3[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Wed Apr 5 13:33:42 2017 -0400
+
+    EndlessBattle
+
+[33mcommit 442db50236c42904385e9f19a5a3593ea6e2ef7c[m
+Author: Harris Mohsin <Harris Mohsin>
+Date:   Thu Mar 2 14:58:54 2017 -0500
+
+    Main.c updated
+
+[33mcommit 808eda6d4d49db57d436a1072719afb51490ff01[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Tue Feb 28 14:23:44 2017 -0500
+
+    deleted newb
+
+[33mcommit 67c66b8792d7b9bbdb374bdec527489853b1a34f[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Tue Feb 28 14:15:08 2017 -0500
+
+    created newb.txt file
+
+[33mcommit e3172925c7ec9d13fb848425452eb99c6c59f22e[m
+Author: Shaumik-Ashraf <a.shaumik@gmail.com>
+Date:   Wed Feb 22 12:10:22 2017 -0500
+
+    Image fails; put diagram link
+
+[33mcommit f7c20aa702f0da3754982dea5c3257aeb00db8d9[m
+Author: Shaumik-Ashraf <a.shaumik@gmail.com>
+Date:   Wed Feb 22 12:09:48 2017 -0500
+
+    adding diagram to readme
+
+[33mcommit 03854b6e71dcc0cb8efcd48c87e0b4185897f8ab[m
+Author: Shaumik-Ashraf <a.shaumik@gmail.com>
+Date:   Wed Feb 22 12:08:35 2017 -0500
+
+    added diagram to readme
+
+[33mcommit 03f0df9b749384a3c80acaca8f20c8329c95c921[m
+Merge: 391f7e9 fd990ae
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Feb 8 16:38:09 2017 -0500
+
+    Merge branch 'master' of https://github.com/HarrisMohsin/EndlessBattle
+
+[33mcommit 391f7e930d1fa7c762365a9cf4726588930f752d[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Wed Feb 8 16:38:01 2017 -0500
+
+    Added diagram
+
+[33mcommit fd990ae82695b56b3e1244614e9077d2e30cbdff[m
+Author: Shaumik-Ashraf <a.shaumik@gmail.com>
+Date:   Sun Feb 5 12:09:01 2017 -0500
+
+    fixed more formatting and typos in readme
+
+[33mcommit 8bf8575b2f4698c6e2971a05243b29a1b68f008c[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Sun Feb 5 12:07:07 2017 -0500
+
+    fixed markdown formatting in readme
+
+[33mcommit 5a670999366a306e0ff101883d05fd3af093ee1f[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Sun Feb 5 11:38:55 2017 -0500
+
+    Update README.md
+
+[33mcommit 0830485591dbe8cc26d793194485eb29ff459b0e[m
+Merge: a86d5ef f96ca64
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Sun Feb 5 11:36:15 2017 -0500
+
+    Merge branch 'master' of https://github.com/HarrisMohsin/EndlessBattle
+
+[33mcommit a86d5ef484e66a7569abfc6b6b6a2b207e7c5cd7[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Sun Feb 5 11:36:08 2017 -0500
+
+    added items to design in readme
+
+[33mcommit f96ca6474a4df664be521519d2f645c7f4fcad5f[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Sun Feb 5 11:25:15 2017 -0500
+
+    Update README.md
+
+[33mcommit 93bcaf45cb2745e7f528e5d357ca48b439124d56[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Sun Feb 5 11:09:13 2017 -0500
+
+    Update README.md
+
+[33mcommit f7dfc7021b8b28d92921c8bed738bdcb1eeaa684[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Sun Feb 5 11:02:22 2017 -0500
+
+    Update README.md
+
+[33mcommit cd638aa497704abaf2ba1d5eaa6d928f2f926a78[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Sun Feb 5 10:58:27 2017 -0500
+
+    Update README.md
+
+[33mcommit 92f791eb40c626bf17ce8332069f1e1be31ac9f0[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Sun Feb 5 10:13:37 2017 -0500
+
+    Update README.md
+
+[33mcommit 009d66adbdf7464aebef2f45da4d54d6ae43e9d8[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Sun Feb 5 09:25:43 2017 -0500
+
+    README.md
+
+[33mcommit d785912c2a55adf3da37b06b49adb1ab9cece20d[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Fri Jan 20 22:27:47 2017 -0500
+
+    Update main.c
+
+[33mcommit 1c2a8dee3c1ca6e1a94917d7f9dc9d3a49d7da6b[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Fri Jan 20 16:05:44 2017 -0500
+
+    Update main.c
+
+[33mcommit 4fe8ae9940ee0015faee878fb16c257e081dbd17[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Fri Jan 20 16:00:46 2017 -0500
+
+    Update main.c
+
+[33mcommit beed0aabaf1162ff0ebd7a16b4166db56d8ee8ed[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Fri Jan 20 15:54:35 2017 -0500
+
+    Update README.md
+
+[33mcommit 4baec1d6cf12bfd5c5dcfe8a490aa24d8cb88693[m
+Author: Shaumik Ashraf <a.shaumik@gmail.com>
+Date:   Fri Jan 20 15:21:07 2017 -0500
+
+    updated readme and created empty main.c
+
+[33mcommit 88cf5abbb1f3ecc3015ff22d9fa4b40ab3f850e1[m
+Author: Harris Mohsin <harrismo17@gmail.com>
+Date:   Fri Jan 20 15:12:17 2017 -0500
+
+    Create README.md
